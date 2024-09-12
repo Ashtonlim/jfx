ee3b08386fda6e1978ce6e18dfbd5252a988cb2a
ashtonlim <ashtonlim22@gmail.com>
2024-09-12 08:37:20 +0800

    first commit

---
+++ app/src/main/java/org/openjfx/BrowserView.java
@@ -0,0 +1,17 @@
+package org.openjfx;
+
+// import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
+// import com.teamdev.jxbrowser.engine.Engine;
+
+public class BrowserView {
+    public void Browser() {
+        System.out.println("Browser constructor");
+        String licenseKey = "OK6AEKNYF27LOP29K9XD04Y24ZWAE3TPS1VO5OQ3N3UFBNXP5GS3MK2JGEGJFR8CRZVURRO59PAFH8P4GXKZ28B99MT5S8TLT5SINFDZFKPZ74ZQVPVB6G9WVG7HYLMVL8QZSFUVJ1GFY348T";
+
+        // Engine engine = Engine.newInstance(
+        //                     EngineOptions.newBuilder(HARDWARE_ACCELERATED)
+        //                     .licenseKey("your_license_key")
+        //                     .build());
+
+    }
+}
